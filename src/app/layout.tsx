763fe2027c57ade75071f3cc3a992cbe88f357a6
HIRO15254 <hi089697@gmail.com>
2025-10-23 15:34:08 +0900

    全てのページをdynamic renderingへ

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,6 +53,8 @@ const geist = Geist({
   subsets: ["latin"],
 });
 
+export const dynamic = "force-dynamic"
+
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="ja" {...mantineHtmlProps} suppressHydrationWarning>
